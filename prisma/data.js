39ee6e236681dd9a9de27b73184b854fbc5e670a
Muhammad Izzat Kamaruddin <izzat.kamaruddin@mcmc.gov.my>
2023-10-16 15:16:19 +0800

    Description of the changes

---
+++ prisma/data.js
@@ -0,0 +1,103 @@
+
+const { Prisma } = require('@prisma/client');
+
+ const mbbsub = [
+    {
+        state : 'Malaysia',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Johor',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Kedah',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Kelantan',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Pahang',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+];
+
+const fbbsub = [
+    {
+        state : 'Malaysia',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Johor',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Kedah',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Kelantan',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+    {
+        state : 'Pahang',
+        service : 'Data Only',
+        service_type :'Postpaid',
+        declaration_quarter :3,
+        declaration_year :2019,
+        household :'Household',
+        total_subs :40997,
+    },
+];
+
+module.exports = {
+    mbbsub,
+    fbbsub,
+  };
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,25 @@
 const { PrismaClient } = require('@prisma/client');
-const { subscriptions} = require('./subs.ts');
+const { mbbsub, fbbsub} = require('./data.js');
 const prisma = new PrismaClient();
 
 const load = async () => {
   try {
-    await prisma.category.deleteMany();
-    console.log('Deleted records in category table');
 
-    await prisma.product.deleteMany();
-    console.log('Deleted records in product table');
+    await prisma.mbbsubs.deleteMany();
+    console.log('Deleted records in mbbsubs table');
 
-    await prisma.$queryRaw`ALTER TABLE Product AUTO_INCREMENT = 1`;
-    console.log('reset product auto increment to 1');
+    await prisma.fbbsubs.deleteMany();
+    console.log('Deleted records in fbbsubs table');
 
-    await prisma.$queryRaw`ALTER TABLE Category AUTO_INCREMENT = 1`;
-    console.log('reset category auto increment to 1');
-
-    await prisma.category.createMany({
-      data: subscriptions,
+    await prisma.mbbsubs.createMany({
+      data: mbbsub,
     });
     console.log('Added subscriptions data');
+
+    await prisma.fbbsubs.createMany({
+        data: fbbsub,
+      });
+      console.log('Added subscriptions data');
 
   } catch (e) {
     console.error(e);
